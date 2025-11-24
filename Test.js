a154bc67dd14d71638bcc78326f8d7d1fffca985
KferrandonFulbert <98470782+kferrandonFulbert@users.noreply.github.com>
2025-11-24 10:36:07 +0100

    first add

--- Test.js
+++ Test.js
@@ -1,4 +1,4 @@
-const { addition } = require("/index");
+const { addition } = require("./index");
 const result = addition(2, 3);
 if (result === 5) {
  console.log("✔ Test réussi : addition(2,3) = 5");
